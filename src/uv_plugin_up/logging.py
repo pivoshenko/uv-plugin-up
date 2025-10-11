a73630e78381c136ccfad1091dd4bac26fede601
pivoshenko <volodymyr.pivoshenko@gmail.com>
2025-10-11 14:46:15 +0100

    refactor: implement singleton pattern for Logger class

--- src/uv_plugin_up/logging.py
+++ src/uv_plugin_up/logging.py
@@ -2,10 +2,21 @@
 
 from __future__ import annotations
 
+import typing
+
 import click
 
 
-class Logger:
+class SingletonMeta(type):
+    _instances: typing.ClassVar[dict[type, object]] = {}
+
+    def __call__(cls, *args: object, **kwargs: object) -> object:
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Logger(metaclass=SingletonMeta):
     def info(self, message: str) -> None:
         self._log(click.style(message, fg="green"))
 
